d8c8c878c29fb92b60baaee0984ae6601127f6d8
Saad Umar <saad_umar93@yahoo.com>
2023-04-05 15:45:31 +0500

    Initial Commit

---
+++ RetryButton/RetryButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetryButtonApp.swift
+//  RetryButton
+//
+//  Created by Tixsee on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct RetryButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
